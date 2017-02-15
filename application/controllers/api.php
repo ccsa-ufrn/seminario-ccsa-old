2eb43438f90ef36c3ac66083ac7d9ed5d318588e
Maradona Morais <maradona.morais@hotmail.com>
2017-02-15 17:11:28 -0300

    Implements message() with TODO issue

---
+++ application/controllers/api.php
@@ -0,0 +1,95 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+# >> Seminário CCSA - Assessoria Técnica
+# > Contributor: Maradona Morais (maradona.morais@hotmail.com)
+# > Last update: 2017-02-15 17:05:00 by Maradona Morais
+# > TODO 1 Read from helper the datetime and increase the $message->created_at
+#   on message() function.
+
+
+/* API Class. Expects REST requests and respond to them with JSON object */
+class API extends CI_Controller {
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /*
+     * Main API endpoint. Useless
+     */
+    public function index() {
+      $this->load->library(array('output'));
+      $this->output
+        ->set_content_type('application/json')
+        ->set_status_header(200) // OK
+        ->set_output(json_encode(array('foo'=>'bar')));
+    }
+
+    /*
+     * Message receiver. Expects:
+     * @param name user name
+     * @param email user mail
+     * @param subject message subject
+     * @param message user message
+     * @return a success|error json obj
+     *
+     * Endpoint: http://URL_TO_CCSA/index.php?/api/message
+     */
+    public function message() {
+      $this->load->library(array('output', 'rb'));
+      $this->output->set_content_type('application/json', 'utf-8');
+
+      $fields = array('name', 'email', 'subject', 'message');
+
+      /* validating fields */
+      foreach ($fields as $field) {
+        if(empty($this->input->post($field))) {
+          $this->output->set_status_header(400) // Bad Request
+          ->set_output(json_encode(array(
+            'status'=>'error',
+            'message'=>'Campo(s) obrigatório(s) não preenchido(s)'
+          )));
+          break;
+        }
+      }
+
+      /* create RB instance and data terms */
+      $message = R::dispense('message');
+      foreach ($fields as $field) {
+        /* this fits only name, email, subject and message */
+        $message[$field] = $this->input->post($field);
+      }
+      $message->created_at = '2017-02-15 16:44:46'; #TODO 1
+      $message->answered = 'no';
+
+      /* store it at database */
+      try {
+        $id = R::store($message);
+
+        /* mount data object to return*/
+        $data = array(
+          'id'=>$id,
+          'name'=>$message->name,
+          'email'=>$message->email,
+          'subject'=>$message->subject,
+          'message'=>$message->message
+        );
+
+        /* returns status and data */
+        $this->output->set_status_header(200)
+          ->set_output(json_encode(array(
+            'status'=>'success',
+            'message'=>'Mensagem enviada com sucesso',
+            'data'=> $data
+          )));
+      } catch(Exception $e) {
+        /* handle the error */
+        $this->output->set_status_header(500) // Internal Server Error
+          ->set_output(json_encode(array(
+            'status'=>'error',
+            'message'=>'Ocorreu um erro de conexão ao banco de dados'
+          )));
+      }
+
+    }
+
+}
