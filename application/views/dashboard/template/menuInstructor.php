839a78418500b0fd0868b54eae66fd5e94404697
Victor Almeida Schinaider <victorschinaider@outlook.com>
2017-04-12 17:11:15 -0300

    close #17

--- application/views/dashboard/template/menuInstructor.php
+++ application/views/dashboard/template/menuInstructor.php
@@ -13,28 +13,32 @@
                         <li>
                             <a <?php if($active=='payment'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/user/payment'); ?>"><i class="fa fa-money fa-fw"></i> Pagamento</a>
                         </li>
-                        
-                        <?php 
-                            $user = R::findOne('user', 'id = ?', array($this->session->userdata('user_id'))); 
-                            if(count($user->ownMinicourseList) || count($user->ownRoundtableList) || count($user->ownWorkshopList)) :  
+
+                        <?php
+                            $user = R::findOne('user', 'id = ?', array($this->session->userdata('user_id')));
+                            if(count($user->ownMinicourseList) || count($user->ownRoundtableList) || count($user->ownWorkshopList)) :
                         ?>
-                        
+
                             <li>
                                 <a href="<?php echo base_url('dashboard/minicourse/my-minicourses'); ?>"><i class="fa fa-inbox" aria-hidden="true"></i> Meus Trabalhos</a>
                             </li>
-                            
+
                         <?php endif; ?>
-                        
+
                         <li>
                             <a <?php if($active=='minicourseenroll'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/minicourse/enroll'); ?>"><i class="fa fa-pencil"></i> Inscrever-se em Minicursos</a>
                         </li>
-                        
+
                         <li>
                             <a <?php if($active=='roundtableenroll'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/roundtable/enroll'); ?>"><i class="fa fa-pencil"></i> Inscrever-se em Mesas-Redondas</a>
                         </li>
-                        
+
                         <li>
                             <a <?php if($active=='conferenceenroll'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/conference/enroll'); ?>"><i class="fa fa-pencil"></i> Inscrever-se em Conferências</a>
+                        </li>
+
+                        <li>
+                            <a <?php if($active=='workshopenroll'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/workshop/enroll'); ?>"><i class="fa fa-pencil"></i> Inscrever-se em Oficinas</a>
                         </li>
 
                         <li>
@@ -44,7 +48,7 @@
                         <li>
                             <a <?php if($active=='submit-poster'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/poster/submit'); ?>"><i class="fa fa-upload fa-fw"></i> Pôsteres</a>
                         </li>
-                        
+
                         <li>
                             <a <?php if($active=='submit-teaching-cases'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/teachingcases/submit'); ?>"><i class="fa fa-upload fa-fw"></i> Casos para Ensino</a>
                         </li>
@@ -60,7 +64,7 @@
                         <li>
                             <a <?php if($active=='submit-roundtable'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/roundtable/submit'); ?>"><i class="fa fa-upload fa-fw"></i> Mesas-Redondas</a>
                         </li>
-                        
+
                         <li>
                             <a <?php if($active=='issue'): ?> class="active" <?php endif; ?> href="<?php echo base_url('dashboard/issue/create'); ?>"><i class="fa fa-bug fa-fw"></i> Suporte</a>
                         </li>
@@ -69,7 +73,7 @@
                 </div>
                 <!-- /.sidebar-collapse -->
             </div>
-            
+
             <!-- /.navbar-static-side -->
         </nav>
 
